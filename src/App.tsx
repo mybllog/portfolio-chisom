6b68de3caa16bb95a3f4c689889274ae51df9d79
mybllog <71851445+mybllog@users.noreply.github.com>
2024-04-19 09:59:21 +0100

    Add files via upload
    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Resume from "./components/Resume";
+import Particle from "./components/Particles";
 //import Testimonial from "./components/Testimonial";
-
+import LoadingTimer from "./components/Loading";
 function App() {
   return (
     <main className="font-bodyFont w-full h-auto bg-bodyColor text-lightText">
       <Navbar />
       <div className="px-4">
         <div className="max-w-screen-xl mx-auto">
+          <LoadingTimer duration={10}/>
+          <Particle />
           <Banner />
           <Feature />
           <Projects />
